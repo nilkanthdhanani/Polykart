1a9a736a649360fd4c1955c926d79907e9be9f16
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-13 12:59:37 +0530

    home page add

---
+++ src/components/modals/homeBuySell/homeSell.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./homeBuySell.scss";
+import cross from "../../../assets/icons/cross.svg";
+
+const buyData = [
+  { number: 1, grade: "WH034N", price: "₹ 550,05", qty: 156 },
+  { number: 2, grade: "R03RR", price: "₹ 540,25", qty: 211 },
+  { number: 3, grade: "R103", price: "₹ 542,63", qty: 186 },
+  { number: 4, grade: "HR003", price: "₹ 533,75", qty: 174 },
+  { number: 5, grade: "RH03", price: "₹ 485,44", qty: 114 },
+  { number: 6, grade: "H030SG", price: "₹ 100,44", qty: 142 },
+  { number: 7, grade: "SS35N", price: "₹ 305,44", qty: 115 },
+  { number: 8, grade: "WH040N", price: "₹ 106,44", qty: 520 },
+  { number: 9, grade: "P1045RG", price: "₹ 620,44", qty: 320 },
+  { number: 10, grade: "HR005T", price: "₹ 530,44", qty: 121 },
+  { number: 11, grade: "1030RGw", price: "₹ 530,30", qty: 236 },
+]
+
+export default function HomeSell({ closeModal }) {
+  const handleModalContentClick = (event) => {
+    event.stopPropagation();
+  };
+  return (
+    <div>
+      <div className="home-modal" onClick={closeModal}>
+        <div className="home-modal-box-main">
+          <div className="home-modal-box-content">
+            <div className="home-modal-box" onClick={handleModalContentClick}>
+              <div className="home-modal-box-title">
+                <img src={cross} alt="cross" onClick={closeModal} />
+              </div>
+              <div className="home-modal-table">
+                <table className="sell">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Grades</th>
+                      <th><span>Price</span><span>Heighest Price</span></th>
+                      <th>Qty.(In tons)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {buyData.map((data, index) => (
+                      <tr key={index}>
+                        <td>{data.number}</td>
+                        <td>{data.grade}</td>
+                        <td>{data.price}</td>
+                        <td>{data.qty}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/module/home/index.js
+++ src/module/home/index.js
@@ -1,10 +1,150 @@
-import React from 'react'
-import './home.scss'
+import React, { useState, useEffect } from "react";
+import "./home.scss";
+import HomeBuy from "../../components/modals/homeBuySell/homeBuy";
+import HomeSell from "../../components/modals/homeBuySell/homeSell";
+import Searchicon from "../../assets/icons/searchicon.svg";
 
-export default function index() {
+const data1 = [
+  { commodity: "Raffia", date: "Sep 05", qty: 156, price: "₹ 550,05" },
+  { commodity: "Mould Basell", date: "Sep 05", qty: 211, price: "₹ 540,25" },
+  { commodity: "Raffia Opal", date: "Sep 05", qty: 186, price: "₹ 542,63" },
+  { commodity: "Raffia Ril", date: "Sep 05", qty: 174, price: "₹ 533,75" },
+  { commodity: "PP THERMO", date: "Sep 05", qty: 114, price: "₹ 485,44" },
+  { commodity: "Thermo Ril", date: "Sep 05", qty: 142, price: "₹ 100,44" },
+  { commodity: "Thermo Mrpl", date: "Sep 05", qty: 115, price: "₹ 305,44" },
+  { commodity: "PP TQ", date: "Sep 05", qty: 520, price: "₹ 106,44" },
+  { commodity: "Tq Iocl", date: "Sep 05", qty: 320, price: "₹ 620,44" },
+  { commodity: "Tq Bcpl", date: "Sep 05", qty: 121, price: "₹ 530,44" },
+];
+
+const data2 = [
+  { price: "₹ 402,05", qty: 156, commodity: "Raffia", date: "Sep 05" },
+  { price: "₹ 340,25", qty: 211, commodity: "Mould Basell", date: "Sep 05" },
+  { price: "₹ 133,75", qty: 174, commodity: "Raffia Ril", date: "Sep 05" },
+  { price: "₹ 185,44", qty: 114, commodity: "PP THERMO", date: "Sep 05" },
+  { price: "₹ 100,14", qty: 142, commodity: "Thermo Ril", date: "Sep 05" },
+  { price: "₹ 205,44", qty: 115, commodity: "Thermo Mrpl", date: "Sep 05" },
+  { price: "₹ 102,44", qty: 520, commodity: "PP TQ", date: "Sep 05" },
+  { price: "₹ 320,44", qty: 320, commodity: "Tq Iocl", date: "Sep 05" },
+  { price: "₹ 230,44", qty: 121, commodity: "Tq Bcpl", date: "Sep 05" },
+];
+
+export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState("");
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.classList.add("scroll-off");
+    } else {
+      document.body.classList.remove("scroll-off");
+    }
+
+    return () => {
+      document.body.classList.remove("scroll-off");
+    };
+  }, [isModalOpen]);
+
+  const openModal = (type) => {
+    setModalType(type);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalType("");
+  };
+
   return (
-    <>
-     <div className="home-grid"></div>
-    </>
-  )
+    <div className="home-page">
+      <div className="search-input-main">
+        <input placeholder="Search eg; (BCPL, HMEL, HPL etc)" />
+        <div className="searchicon-main">
+          <img src={Searchicon} alt="Searchicon" />
+        </div>
+      </div>
+      <div className="home-grid">
+        <div className="home-table1">
+          <table>
+            <thead>
+              <tr>
+                <th>Commodity</th>
+                <th>Qty.<span>(In tons)</span></th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data1.map((item, index) => (
+                <tr key={index} onClick={() => openModal("buy")}>
+                  <td>
+                    <div className="commodity-name">
+                      <h2>{item.commodity}</h2>
+                      <span>{item.date}</span>
+                    </div>
+                  </td>
+                  <td>{item.qty}</td>
+                  <td>{item.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="home-table2">
+          <table>
+            <thead>
+              <tr>
+                <th>Price</th>
+                <th>Qty.<span>(In tons)</span></th>
+                <th>Commodity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data2.map((item, index) => (
+                <tr key={index} onClick={() => openModal("sell")}>
+                  <td>{item.price}</td>
+                  <td>{item.qty}</td>
+                  <td>
+                    <div className="commodity-name">
+                      <h2>{item.commodity}</h2>
+                      <span>{item.date}</span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className="home-table2 respo">
+          <table>
+            <thead>
+              <tr>
+                <th>Commodity</th>
+                <th>Qty.<span>(In tons)</span></th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data2.map((item, index) => (
+                <tr key={index} onClick={() => openModal("sell")}>
+                  <td>
+                    <div className="commodity-name">
+                      <h2>{item.commodity}</h2>
+                      <span>{item.date}</span>
+                    </div>
+                  </td>
+                  <td>{item.qty}</td>
+                  <td>{item.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        {isModalOpen && modalType === "buy" && <HomeBuy closeModal={closeModal} />}
+        {isModalOpen && modalType === "sell" && <HomeSell closeModal={closeModal} />}
+      </div>
+    </div>
+  );
 }
